024699527819cab3d15813c04209305185b94495
Aditya Kumar Jha <adityajha126@gmail.com>
2019-06-30 18:27:39 +0530

    ways

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const gitexp;
+console.log('Before');
+const user = getUser(1);
+console.log(user);
+console.log('After')
+
+function getUser(id) {
+   setTimeout( () => {
+     console.log('reading a user');
+     return{ id: id , githubUserName : 'AdityaKumarJha'}, 2000 );
+   return 1;
+}
+
+//three ways to deal with async javascript callbacks, promices ,await/async
